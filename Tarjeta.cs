452c53e91fdb42d29c2cceab3872613181e0dc33
Nicolás Gómez <nicolas@mtblc.co>
2019-04-10 18:27:39 -0400

    :tada: incremento 1

---
+++ Tarjeta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Laboratorio4
+{
+    public abstract class Tarjeta
+    {
+        int saldo;
+        int id;
+
+        public Tarjeta(int saldoInicial, int ID)
+        {
+            saldo = saldoInicial;
+            id = ID;
+        }
+
+        public int GetSaldo()
+        {
+            return saldo;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        protected abstract int GetTarifa(int hora, int minutos);
+
+        public bool Pagar(int hora, int minutos) 
+        {
+            int tarifaActual = GetTarifa(hora, minutos);
+            int nuevoSaldo = saldo - tarifaActual;
+            if (nuevoSaldo >= 0) 
+            {
+                saldo = nuevoSaldo;
+                return true;
+            }
+            return false;
+        }
+    }
+}
